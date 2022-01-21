c776aa255f0fa39cdaf59c074c99072274d3ff73
Smarty2609 <saini.shivam9286@gmail.com>
2022-01-21 12:58:18 +0530

    working fine

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {increment,decrement} from '../actions/action';
+import {useSelector,useDispatch} from 'react-redux';
 
 function App() {
 
+ const counter = useSelector(state => state);
+ const dispatch = useDispatch();
   return (
     <div id='main'>
-      
-    </div>
+      <h2 data-testid='counter'>{counter}</h2>
+      <button onClick={()=> dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div> 
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import countReducer from './reducers/index';
 
+const store = createStore(countReducer);
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider> 
+    ,
+  document.getElementById('root')
+);
 
