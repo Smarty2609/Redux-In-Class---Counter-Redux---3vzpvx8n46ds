6736aecec0e42e33e000739fd04762e463026b58
Shiv@m69996 <52376041+Smarty2609@users.noreply.github.com>
2022-01-21 13:47:56 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 
-const countReducer = (state = 0, action) =>{
+const countReducer = (state = {counter:0}, action) =>{
    switch(action.type){ 
     case 'INCREMENT':
-       return state + 1;
+       return {counter: state.counter + 1};
     
     case 'DECREMENT':
-       return state - 1;
+       return {counter:state.counter - 1};
     
    default:
       return state;
